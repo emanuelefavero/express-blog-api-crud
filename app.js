1b2818cad629ebf01cd1684f4524b8bbeabee4f2
Emanuele Favero <emafaux89@gmail.com>
2026-09-05 23:16:16 +0200

    feat: add express json middleware to read req.body

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const PORT = process.env.PORT ?? 3000;
 
 const app = express();
 
+app.use(express.json());
+
 app.use(express.static(path.join(import.meta.dirname, 'public')));
+
 registerPosts(app);
 registerRoot(app);
+
 app.use((_, res) => res.status(404).json({ message: 'Not Found' })); // 404
 
 app.listen(PORT, () => {
